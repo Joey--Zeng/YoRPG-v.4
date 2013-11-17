d0de9f3cd0e41ec4fdc1aa166d7e45e31abf042d
Joey--Zeng <jzengstuy@gmail.com>
2013-11-16 23:35:20 -0500

    Update Rogue.java

--- Rogue.java
+++ Rogue.java
@@ -5,4 +5,31 @@
 
 
 
-public class Rogue 
+public class Rogue extends Character{
+  
+  public Rogue(){
+      name = "asd";
+      health = 40;
+      strength = 80;
+      defense = 100;
+      attack = 1.8;
+  }
+
+  public Rogue(String a){
+      this();
+      name = a;
+  }
+  
+  public void specialize(){
+      attack = 3.2;
+      defense = 50;
+  }
+  
+  public void normalize(){
+      attack = 1.8;
+      defense = 100;
+  }
+  
+ 
+  
+}
